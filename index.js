9d78efd2b52868a49b57d5ca32844427bc03bca1
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-07-05 14:16:41 -0300

    Cata Blank and Emojis

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ function closeNav(){
 function changeText(x){
     const languages = {
         html: () => {
-            document.getElementById("text1").innerHTML = "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+            document.getElementById("text1").innerHTML = "💻 HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
             document.getElementById("text2").innerHTML = "2 anos de experiência."
         },
         css: () => {
-            document.getElementById("text1").innerHTML = 'CSS é uma linguagem de folha de estilo composta por "camadas", criado com o propósito de estilizar as páginas HTML.'
+            document.getElementById("text1").innerHTML = '👩‍🎨CSS é uma linguagem de folha de estilo composta por "camadas", criado com o propósito de estilizar as páginas HTML.'
             document.getElementById("text2").innerHTML = "2 anos de experiência."
         },
         js: () => {
-            document.getElementById("text1").innerHTML = 'JavaScript é uma linguagem de programação de script em alto nível, é utilizado principalmente para atribuir funções aos elementos de uma página.'
+            document.getElementById("text1").innerHTML = '👩‍💻JavaScript é uma linguagem de programação de script em alto nível, é utilizado principalmente para atribuir funções aos elementos de uma página.'
             document.getElementById("text2").innerHTML = "6 meses de experiência."
         },
         java: () => {
-            document.getElementById("text1").innerHTML = 'Java é uma linguagem de programação de alto nível, baseada em classes, orientada a objetos e muito conhecida por sua portabilidade e segurança.'
+            document.getElementById("text1").innerHTML = '☕Java é uma linguagem de programação de alto nível, baseada em classes, orientada a objetos e muito conhecida por sua portabilidade e segurança.'
             document.getElementById("text2").innerHTML = "6 meses de experiência."
         },
         python: () => {
-            document.getElementById("text1").innerHTML = 'Python é uma linguagem de programação de alto nível muito conhecida por possuir um código de fácil leitura e por poder ser utilizada em várias áreas de desenvolvimento.'
+            document.getElementById("text1").innerHTML = '🐍Python é uma linguagem de programação de alto nível muito conhecida por possuir um código de fácil leitura e por poder ser utilizada em várias áreas de desenvolvimento.'
             document.getElementById("text2").innerHTML = "3 meses de experiência."
         },
         "c++": () => {
-            document.getElementById("text1").innerHTML = 'C++ é uma linguagem de programação de alto nível, orientada a objetos, criada como extensão da linguagem C. É muito conhecida por possuir altos níveis de desempenho, performance e eficiência.'
+            document.getElementById("text1").innerHTML = '👾 C++ é uma linguagem de programação de alto nível, orientada a objetos, criada como extensão da linguagem C. É muito conhecida por possuir altos níveis de desempenho, performance e eficiência.'
             document.getElementById("text2").innerHTML = "2 anos de experiência."
         }
     }
